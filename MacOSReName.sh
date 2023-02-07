0224594054bcac375bb91420c150b221c6b14645
suchartmart <suchartmart@gmail.com>
2023-02-07 15:14:20 +0700

    Delete MacOSReName.sh

--- MacOSReName.sh
+++
@@ -1,5 +0,0 @@
-theSerial=$(system_profiler SPHardwareDataType | awk '/Serial/ {print $4}')
-
-sudo scutil --set ComputerName "AGC-$theSerial"
-sudo scutil --set HostName "AGC-$theSerial"
-sudo scutil --set LocalHostName "AGC-$theSerial"
